e607bf71d37872d8fe7449d7ccfa740f75bf2d74
Sarah Barajas <sarahbarajas91@yahoo.com>
2024-10-27 20:44:19 -0700

    Outputs a triangle of hashtags to the console

--- script.js
+++ script.js
@@ -7,3 +7,17 @@
 // ###
 // ##
 // #
+function displayTriangle() {
+    const maxRows = 4; //triangle display to the console with  max rows set to 4
+  
+    for (let i = 1; i <= maxRows; i++) {
+      console.log('#'.repeat(i));
+    } // Increments i until it is less than or equal to maxRows, then repeats the "#" 4 times (represented by "i")
+  
+    for (let i = maxRows - 1; i >= 1; i--) {
+      console.log('#'.repeat(i));
+    }
+    
+displayTriangle(); You
+
+// Outputs a triangle of hashtags to the console
